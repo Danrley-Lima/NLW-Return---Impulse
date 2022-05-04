ab2a9e8d2cedbd348d9010158b82ab7aaa5df07e
Danrley Lima <danrleylac@gmail.com>
2022-05-03 21:27:39 -0300

    Fim da primeira aula

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,17 +1,25 @@
 import { ChatTeardropDots } from "phosphor-react"
+// import { useState } from "react"
+import { Popover } from '@headlessui/react'
 
 export function Widget() {
-    return (
-        <div className="absolute bottom-5 right-5">
-            <p>Hello Word</p>
+    // const [isWidgetOpen, setIsWidgetOpen] = useState(false)
 
-            <button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+    // function toggleWidgetVisibility() {
+    //     setIsWidgetOpen(!isWidgetOpen)
+    // }
+
+    return (
+        <Popover className="absolute bottom-5 right-5">
+            {/* { isWidgetOpen && <p>Hello World</p> } operador && só executa o segundo lado da operação se o primeiro for true */}
+            <Popover.Panel>Hello World</Popover.Panel>
+            <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
             <ChatTeardropDots className="w-6 h-6"/>
 
             <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
                 Feedback
             </span>
-            </button>
-        </div>
+            </Popover.Button>
+        </Popover>
     )
 }
