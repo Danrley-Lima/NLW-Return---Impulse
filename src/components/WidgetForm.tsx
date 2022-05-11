9cd5be387ea6a640b7393b8ee9b2c62b4126a362
Danrley Lima <danrleylac@gmail.com>
2022-05-11 17:13:35 -0300

    Bug no useState

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -2,6 +2,7 @@ import { CloseButton } from "./CloseButton";
 import bugImageUrl from '../assets/bug.svg'
 import ideaImageUrl from '../assets/idea.svg'
 import thoughtImageUrl from '../assets/thought.svg'
+import { useState } from "react";
 
 export const feedbackTypes = {
     BUG: {
@@ -28,6 +29,8 @@ export const feedbackTypes = {
 }
 
 export function WidgetForm() {
+    // const [feedbackType, setFeedbackType] = useState(null)
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             <header>
@@ -40,8 +43,9 @@ export function WidgetForm() {
                 { Object.entries(feedbackTypes).map(([key, value]) => {
                     return (
                         <button
+                            key={key}
                             className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                            // onClick={}
+                            onClick={() => {}}
                             type="button"
                         >
                             <img src={value.image.source} alt={value.image.alt} />
