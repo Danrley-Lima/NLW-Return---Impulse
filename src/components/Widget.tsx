e0ed025f1a02415be647c6ceac5f1db4e9bdee8d
Danrley Lima <danrleylac@gmail.com>
2022-05-11 17:10:18 -0300

    Adicionando icones e estilização no widget

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,25 +1,22 @@
 import { ChatTeardropDots } from "phosphor-react"
-// import { useState } from "react"
 import { Popover } from '@headlessui/react'
 import { WidgetForm } from "./WidgetForm"
+import { useState } from "react"
 
 export function Widget() {
-    // const [isWidgetOpen, setIsWidgetOpen] = useState(false)
-
-    // function toggleWidgetVisibility() {
-    //     setIsWidgetOpen(!isWidgetOpen)
-    // }
 
     return (
-        <Popover className="absolute bottom-5 right-5 flex flex-col items-end">
-            {/* { isWidgetOpen && <p>Hello World</p> } operador && só executa o segundo lado da operação se o primeiro for true */}
-            <Popover.Panel>{ WidgetForm }</Popover.Panel>
+        <Popover className="absolute bottom-4 right-4 md:bottom-10 md:right-10 flex flex-col items-end">
+            <Popover.Panel>
+                {WidgetForm}
+            </Popover.Panel>
             <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
-            <ChatTeardropDots className="w-6 h-6"/>
+                <ChatTeardropDots className="w-6 h-6" />
 
-            <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
-                Feedback
-            </span>
+                <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+                    <span className="pl-2"></span>
+                    Feedback
+                </span>
             </Popover.Button>
         </Popover>
     )
